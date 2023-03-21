51b92391dfbc629546c9e033bb24f5be531e4729
samhitha16 <p.samhitharao22@gmail.com>
2023-03-21 23:21:04 +0530

    Updating All Functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,36 +13,38 @@ public class App {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Choose your function:\n"
-                + "\t1. Sum\n"
-                + "\t2. Subtract\n"
-                + "\t3. Multiply\n"
-                + "\t4. Divide\n"
-                + "\t5. Natural log\n"
-                + "\t6. Square\n"
-                + "\t7. Square root\n"
-                + "\t8. Exponent\n" +
-                "\t9. Factorial");
+                + "\t1. Square Root\n"
+                + "\t2. Factorial\n"
+                + "\t3. Natural Log\n"
+                + "\t4. Exponent\n");
+                // + "\t5. Natural log\n"
+                // + "\t6. Square\n"
+                // + "\t7. Square root\n"
+                // + "\t8. Exponent\n" +
+                // "\t9. Factorial");
         Integer funcID = input.nextInt();
         Integer number1=0;
         Integer number2=0;
         System.out.println("Enter Input: ");
         number1 = input.nextInt();
-        if(funcID == 1 || funcID == 2 || funcID == 3 ||
-                funcID == 4 || funcID == 8)
+        // if(funcID == 1 || funcID == 2 || funcID == 3 ||
+        //         funcID == 4 || funcID == 8)
+        if(funcID == 4)
         {
             number2 = input.nextInt();
         }
 
         switch(funcID) {
-            case 1: System.out.println(Calculator.sum(number1, number2));break;
-            case 2: System.out.println(Calculator.subtract(number1, number2));break;
-            case 3: System.out.println(Calculator.multiply(number1, number2));break;
-            case 4: System.out.println(Calculator.divide(number1, number2));break;
-            case 5: System.out.println(Calculator.naturalLogarithm(number1));break;
-            case 6: System.out.println(Calculator.square(number1));break;
-            case 7: System.out.println(Calculator.squareRoot(number1));break;
-            case 8: System.out.println(Calculator.Exponent(number1, number2));break;
-            case 9: System.out.println(Calculator.factorial(number1));break;
+            // case 1: System.out.println(Calculator.sum(number1, number2));break;
+            // case 2: System.out.println(Calculator.subtract(number1, number2));break;
+            // case 3: System.out.println(Calculator.multiply(number1, number2));break;
+            // case 4: System.out.println(Calculator.divide(number1, number2));break;
+            case 1: System.out.println(Calculator.squareRoot(number1));break;
+            // case 2: System.out.println(Calculator.square(number1));break;
+            case 2: System.out.println(Calculator.factorial(number1));break;
+            case 3: System.out.println(Calculator.naturalLogarithm(number1));break;
+            case 4: System.out.println(Calculator.Exponent(number1, number2));break;
+            
             default: System.out.println("Invalid input");break;
         }
 
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -16,47 +16,47 @@ public class AppTest {
 //    Calculator calculator;
 
     double delta = 1e-15;
-    @Test
-    public void testing_sum() {
-        double expectedValue = 5;
-        double actualValue = Calculator.sum(2, 3);
-        assertEquals("This should evaluate to 5", expectedValue, actualValue, delta);
-    }
+    // @Test
+    // public void testing_sum() {
+    //     double expectedValue = 5;
+    //     double actualValue = Calculator.sum(2, 3);
+    //     assertEquals("This should evaluate to 5", expectedValue, actualValue, delta);
+    // }
 
-    @Test
-    public void testing_subtract() {
-        double expectedValue = 10;
-        double actualValue = Calculator.subtract(15, 5);
-        assertEquals("This should evaluate to 10", expectedValue, actualValue, delta);
-    }
+    // @Test
+    // public void testing_subtract() {
+    //     double expectedValue = 10;
+    //     double actualValue = Calculator.subtract(15, 5);
+    //     assertEquals("This should evaluate to 10", expectedValue, actualValue, delta);
+    // }
 
-    @Test
-    public void testing_divide() {
-        double expectedValue = 10;
-        double actualValue = Calculator.divide(20, 2);
-        assertEquals("This should evaluate to 10", expectedValue, actualValue, delta);
-    }
+    // @Test
+    // public void testing_divide() {
+    //     double expectedValue = 10;
+    //     double actualValue = Calculator.divide(20, 2);
+    //     assertEquals("This should evaluate to 10", expectedValue, actualValue, delta);
+    // }
 
-    @Test
-    public void testing_divide_2() {
-        double expectedValue = -1;
-        double actualValue = Calculator.divide(20, 0);
-        assertEquals("This should evaluate to an error: -1", expectedValue, actualValue, delta);
-    }
+    // @Test
+    // public void testing_divide_2() {
+    //     double expectedValue = -1;
+    //     double actualValue = Calculator.divide(20, 0);
+    //     assertEquals("This should evaluate to an error: -1", expectedValue, actualValue, delta);
+    // }
 
-    @Test
-    public void testing_multiply() {
-        double expectedValue = 100;
-        double actualValue = Calculator.multiply(50, 2);
-        assertEquals("This should evaluate to 100", expectedValue, actualValue, delta);
-    }
+    // @Test
+    // public void testing_multiply() {
+    //     double expectedValue = 100;
+    //     double actualValue = Calculator.multiply(50, 2);
+    //     assertEquals("This should evaluate to 100", expectedValue, actualValue, delta);
+    // }
 
-    @Test
-    public void testing_square() {
-        double expectedValue = 4;
-        double actualValue = Calculator.square(2);
-        assertEquals("This should evaluate to 4", expectedValue, actualValue, delta);
-    }
+    // @Test
+    // public void testing_square() {
+    //     double expectedValue = 4;
+    //     double actualValue = Calculator.square(2);
+    //     assertEquals("This should evaluate to 4", expectedValue, actualValue, delta);
+    //}
 
     @Test
     public void testing_squareRoot() {
